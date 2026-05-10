126a50ba49d86fd956c85f73338239ecfde05511
republicofinternetofficial-blip <republic.of.internet.official@gmail.com>
2026-05-10 17:11:19 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "bitchat-android"
+rootProject.name = "claus"
 include(":app")
 // Using published Arti AAR; local module not included
